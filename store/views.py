a4caf16c151706af76f7051213a15a2bdfe1cc57
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-01-27 17:21:42 +0200

    Pages Templates & Base Layout

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-  return HttpResponse('<h1>Hello Store</h1>')
-  
+  return render(request, 'home.html')  
+
+def aboutPage(request):
+  return render(request, 'about.html')
