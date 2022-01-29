82006328ede855f200b9db628ddb435e37bae391
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-01-29 21:18:40 +0200

    Display Products In Home Page & Slug

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('<slug:category_slug>/', views.home, name= 'products_by_category'),
+  
   # path('about/', views.aboutPage, name='about'),
   path('product/', views.productPage, name='productpage'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
 
 
-def home(request):
-  return render(request, 'home.html')  
+def home(request, category_slug= None):
+  category_page = None
+  products = None
+  if category_slug != None:
+    category_page = get_object_or_404(Category, slug= category_slug)
+    products = Product.objects.filter(category=category_page, available=True)
+  else:
+    products = Product.objects.all().filter(available=True)
+    
+  context = {
+    'category': category_page,
+    'products': products,
+  }
+  
+  return render(request, 'home.html', context)  
 
 def productPage(request):
   return render(request, 'product.html')
