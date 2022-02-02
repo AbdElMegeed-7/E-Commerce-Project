f9b4b259d2fa7a83c6c423af5f02ab18530c4640
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-02-02 12:27:23 +0200

    Cart Model & Func{AddToCart&CartDetail}

--- store/urls.py
+++ store/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('category/<slug:category_slug>/', views.home, name= 'products_by_category'),
-  
   # path('about/', views.aboutPage, name='about'),
   path('category/<slug:category_slug>/<slug:product_slug>/', views.productPage, name='product_detail'),
-  path('cart/', views.cart, name="cart")
+  path('cart/', views.cart_detail, name="cart_detail"),
+  path('cart/add/<int:product_id>/', views.add_cart, name="add_cart"),
 ]
