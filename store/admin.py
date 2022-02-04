e3e475b92a592a2f060b468243c0d9dba924bbbe
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2022-02-04 15:15:32 +0200

    Create Order&OrderItem Model

--- store/admin.py
+++ store/admin.py
@@ -4,15 +4,15 @@ from .models import Category, Product
 
 class CategoryAdmin(admin.ModelAdmin):
   list_display = ['name', 'slug']
-  prepopulated_fields = {'slug', ('name',)}
+  # prepopulated_fields = {'slug', ('name',)}
   
 admin.site.register(Category, CategoryAdmin)
 
 
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ['name', 'price', 'stock', 'available', 'created', 'updated']
+  list_display = ['name', 'price', 'stock', 'available', 'created', 'uploaded']
   list_editable = ['price', 'stock','available']
-  prepopulated_fields = {'slug', ('name',)}
+  # prepopulated_fields = {'slug':('name',)}
   list_per_page = 7
   
 admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from audioop import reverse
 from django.db import models
 from django.urls import reverse
 
+ # Class : Category
 class Category(models.Model):
   name = models.CharField(max_length=250, unique=True)
   slug = models.SlugField(max_length=250, unique=True)
@@ -19,7 +20,7 @@ class Category(models.Model):
   def __str__(self):
       return self.name
     
-
+ # Class : Product
 class Product(models.Model):
   name = models.CharField(max_length=250, unique=True)
   slug = models.SlugField(max_length=250, unique=True)
@@ -43,7 +44,7 @@ class Product(models.Model):
   def __str__(self):
     return self.name
   
-  
+  # Class : Cart
 class Cart(models.Model):
   cart_id = models.CharField(max_length=250, blank=True)
   date_added = models.DateField(auto_now_add=True)
@@ -55,7 +56,7 @@ class Cart(models.Model):
   def __str__(self):
     return self.cart_id
   
-
+# Class CartItem
 class CartItem(models.Model):
   product = models.ForeignKey(Product, on_delete=models.CASCADE)
   cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
@@ -67,6 +68,47 @@ class CartItem(models.Model):
     
   def sub_total(self):
     return self.product.price * self.quantity
+  
+  def __str__(self):
+    return self.product
+  
+# Class : Order
+class Order(models.Model):
+  token = models.CharField(max_length=250, blank=True)
+  total = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD ORDER TOTAL')
+  email_address = models.EmailField(max_length=250, blank=True, verbose_name='EMAIL')
+  created = models.DateTimeField(auto_now_add=True)
+  billing_name= models.CharField(max_length=250, blank=True)
+  billing_address= models.CharField(max_length=250, blank=True)
+  billing_city= models.CharField(max_length=250, blank=True)
+  billing_postcode= models.CharField(max_length=250, blank=True)
+  billing_country= models.CharField(max_length=250, blank=True)
+  shipping_name= models.CharField(max_length=250, blank=True)
+  shipping_address= models.CharField(max_length=250, blank=True)
+  shipping_city= models.CharField(max_length=250, blank=True)
+  shipping_postcode= models.CharField(max_length=250, blank=True)
+  shipping_country= models.CharField(max_length=250, blank=True)
+
+  class Meta:
+    ordering = ['-created']
+    db_table = 'Order'
+    
+  def __str__(self):
+    return str(self.id)
+  
+
+# Class : OrderItem
+class OrderItem(models.Model):
+  product = models.CharField(max_length=250)
+  quantity = models.IntegerField()
+  price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='USD')
+  order = models.ForeignKey(Order, on_delete=models.CASCADE)
+  
+  class Meta:
+    db_table = 'OrderItem'
+  
+  def sub_total(self):
+    return (self.quantity * self.price)
   
   def __str__(self):
     return self.product
